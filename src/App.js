8d4c3b49028669a124b7126b841c91937a0f801d
AnnaMarandola <annamarandola@gmail.com>
2021-04-14 14:53:18 +0200

    login

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import { ChatEngine } from "react-chat-engine";
 import ChatFeed from "./components/ChatFeed";
+import LoginForm from "./components/LoginForm";
 
 import "./App.css";
 
 const App = () => {
+  if (!localStorage.getItem("username")) return <LoginForm />;
+
   return (
     <ChatEngine
       height="100vh"
       projectID="39cba861-c749-45b1-96fb-fea4ef1980c2"
-      userName="Anna"
-      userSecret="wxcvbn"
-      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps}/>}
+      userName={localStorage.getItem("username")}
+      userSecret={localStorage.getItem("password")}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
     />
   );
 };
