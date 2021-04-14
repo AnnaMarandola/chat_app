960d13a3c7f31ffe2d3914750a0382a947b1bc80
AnnaMarandola <annamarandola@gmail.com>
2021-04-14 12:50:32 +0200

    creating chat message components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import { ChatEngine } from 'react-chat-engine';
+import { ChatEngine } from "react-chat-engine";
+import ChatFeed from "./components/ChatFeed";
 
 import "./App.css";
 
 const App = () => {
-    return (
-        <ChatEngine
-            height="100vh"
-            projectID=""
-            userName=""
-            userSecret=""
-        />
-    )
-}
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID="39cba861-c749-45b1-96fb-fea4ef1980c2"
+      userName="Anna"
+      userSecret="wxcvbn"
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps}/>}
+    />
+  );
+};
 
 export default App;
